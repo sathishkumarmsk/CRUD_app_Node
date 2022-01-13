8c934b00fd37d3d9639defccbd5fb721ab5d9dea
sathishkumarmsk <sathishkumarmsk5699@gmail.com>
2022-01-13 20:12:03 +0530

    connect database

--- server.js
+++ server.js
@@ -1,42 +1,29 @@
 const express = require('express');
 const res = require('express/lib/response');
+const MongoClient = require('mongodb').MongoClient
 
 const bodyParser = require('body-parser')
 const app = express();
 
+MongoClient.connect("mongodb+srv://sathish:msk@msk.sxiew.mongodb.net/backend-dev?retryWrites=true&w=majority", { useUnifiedTopology: true })
+.then(client => {
+  console.log('Connected to Database')
+  const db = client.db('star-wars-quotes')
+
+  app.use(bodyParser.urlencoded({ extended: true }))
+
+  app.get('/', (req, res) => {/*...*/ })
+
+  app.post('/quotes', (req,res) => {
+  console.log(req.body)
+  } )
+
+  app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html')
+  })
+})
+.catch(console.error)
 
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
-
-/*
-app.get('/',function(req, res) {
-    res.send('Hello World')
-})
-*/
-
-/*
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
-*/
-
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get('/', (req, res) => {/*...*/ })
-
-app.post('/quotes', (req,res) => {
-    console.log(req.body)
-} )
-
-
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
- 
-
-app.post('/quotes', (req, res) => {
-    console.log('Helloooo!')
-})
-
